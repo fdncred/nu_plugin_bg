1b87ec014341135ce6e5f1db1b10cafad6c462bf
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2023-12-19 12:59:55 -0600

    working-ish

---
+++ README.md
@@ -0,0 +1,34 @@
+# nu_plugin_bg
+
+This is a nushell pluging that attempts to start programs in the background.
+
+## Usage:
+
+```nushell
+❯ bg --help
+Start a process in the background.
+
+Usage:
+  > bg {flags} <command>
+
+Flags:
+  -h, --help - Display the help message for this command
+  -a, --arguments <List(String)> - The arguments of the command.
+  -d, --debug - Debug mode
+
+Parameters:
+  command <string>: The command to start in the background.
+
+Examples:
+  Start a command in the background
+  > some_command --arguments [arg1 --arg2 3]
+```
+
+## Known Issues
+
+If you start a program that writes to stdout, you'll get an error. This what I get on my Mac and I'm not really sure what it means. It could have nothing to do with writing to stdout?
+```nushell
+❯ bg cat -a [cargo.toml] -d
+Starting process: 'cat' with args '["cargo.toml"]'
+cat: cargo.toml: Broken pipe (os error 32)
+```
---
+++ src/main.rs
@@ -0,0 +1,105 @@
+use nu_plugin::{serve_plugin, EvaluatedCall, LabeledError, MsgPackSerializer, Plugin};
+use nu_protocol::{Category, PluginExample, PluginSignature, Span, Spanned, SyntaxShape, Value};
+
+struct Implementation;
+
+impl Implementation {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Plugin for Implementation {
+    fn signature(&self) -> Vec<PluginSignature> {
+        vec![PluginSignature::build("bg")
+            .usage("Start a process in the background.")
+            .required(
+                "command",
+                SyntaxShape::String,
+                "The command to start in the background.",
+            )
+            .named(
+                "arguments",
+                SyntaxShape::List(Box::new(SyntaxShape::String)),
+                "The arguments of the command.",
+                Some('a'),
+            )
+            .switch("debug", "Debug mode", Some('d'))
+            .category(Category::Experimental)
+            .plugin_examples(vec![PluginExample {
+                description: "Start a command in the background".into(),
+                example: "some_command --arguments [arg1 --arg2 3]".into(),
+                result: None,
+            }])]
+    }
+
+    fn run(
+        &mut self,
+        name: &str,
+        call: &EvaluatedCall,
+        _input: &Value,
+    ) -> Result<Value, LabeledError> {
+        assert_eq!(name, "bg");
+        let cmd: Option<Spanned<String>> = call.opt(0)?;
+        let rest: Option<Vec<String>> = call.get_flag("arguments")?;
+        let debug: bool = call.has_flag("debug");
+
+        let ret_val = launch_bg_process(cmd, rest, debug, call.head);
+
+        ret_val
+    }
+}
+
+fn main() {
+    serve_plugin(&mut Implementation::new(), MsgPackSerializer);
+}
+
+pub fn launch_bg_process(
+    cmd: Option<Spanned<String>>,
+    args: Option<Vec<String>>,
+    debug: bool,
+    value_span: Span,
+) -> Result<Value, LabeledError> {
+    if let Some(cmd_name) = cmd {
+        if let Some(cmd_args) = args {
+            if debug {
+                eprintln!(
+                    "Starting process: '{}' with args '{:?}'",
+                    cmd_name.item, cmd_args
+                );
+            }
+            // Start the task as a background child process with arguments
+            let _ = std::process::Command::new(&cmd_name.item)
+                .args(&cmd_args)
+                .spawn()
+                .map_err(|err| LabeledError {
+                    label: "Could not start process".into(),
+                    msg: format!(
+                        "Could not start process name: '{}' with args {:?} {}",
+                        cmd_name.item, cmd_args, err
+                    ),
+                    span: Some(cmd_name.span),
+                })?;
+        } else {
+            if debug {
+                eprintln!("Starting process: '{}', no arguments", cmd_name.item);
+            }
+            // Start the task as a background child process without arguments
+            let _ = std::process::Command::new(&cmd_name.item)
+                .spawn()
+                .map_err(|err| LabeledError {
+                    label: "Could not start process".into(),
+                    msg: format!("Could not start process name: '{}', {}", cmd_name.item, err),
+                    span: Some(cmd_name.span),
+                })?;
+        }
+    } else {
+        return Err(LabeledError {
+            label: "No command provided".into(),
+            msg: "No command provided".into(),
+            span: Some(value_span),
+        });
+    }
+
+    Ok(Value::test_nothing())
+}
